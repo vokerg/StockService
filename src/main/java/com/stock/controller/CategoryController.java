78b64bbeea6c881e8d59f9e580634a5c925ab212
Dmitri Grecov <vokerg@gmail.com>
2018-10-27 22:28:50 +0200

    Category changes

--- src/main/java/com/stock/controller/CategoryController.java
+++ src/main/java/com/stock/controller/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -72,14 +73,28 @@ public class CategoryController {
 		return ResponseEntity.ok(null);
 	}
 
+	@PostMapping("/{id}")
+	public ResponseEntity<Category> updateCategory(@PathVariable String id, @RequestBody Category category,
+			@RequestHeader(value = "idUser", required = true) String idUser) {
+		if (!userService.isAllowedToChangeProduct(idUser)) {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+		}
+		try {
+			Category result = categoryService.updateCategory(id, category);
+			return ResponseEntity.ok(result);
+		} catch (BadRequestException e) {
+			return ResponseEntity.badRequest().body(null);
+		}
+	}
+
 	@GetMapping("/attributes")
 	public List<CategoryAttribute> getAllAttributes() {
 		return categoryAttributeRepository.findAll();
 	}
 
 	@PutMapping("/{id}/attributes")
-	public ResponseEntity<?> insertAttribute(@PathVariable String id, @RequestBody CategoryAttribute attribute,
-			@RequestHeader(value = "idUser", required = true) String idUser) {
+	public ResponseEntity<CategoryAttribute> insertAttribute(@PathVariable String id,
+			@RequestBody CategoryAttribute attribute, @RequestHeader(value = "idUser", required = true) String idUser) {
 
 		if (!userService.isAllowedToChangeProduct(idUser)) {
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
@@ -91,7 +106,7 @@ public class CategoryController {
 		}
 		attribute.setCategory(category);
 		categoryAttributeRepository.save(attribute);
-		return ResponseEntity.ok(null);
+		return ResponseEntity.ok(attribute);
 	}
 
 	@DeleteMapping("/{id}/attributes/{attributeId}")
--- src/main/java/com/stock/service/CategoryService.java
+++ src/main/java/com/stock/service/CategoryService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.stock.entity.Category;
 import com.stock.entity.CategoryAttribute;
-import com.stock.repository.CategoryAttributeProductRepository;
 import com.stock.repository.CategoryRepository;
 
 @Service
@@ -16,17 +15,14 @@ public class CategoryService {
 	@Autowired
 	CategoryRepository catRepository;
 
-	@Autowired
-	CategoryAttributeProductRepository catAttrProdRepo;
-
 	@Transactional
 	public void deleteAttribute(String categoryId, String attributeId) throws BadRequestException {
 		Category category = getCategoryWithException(categoryId);
-		deleteAttribute(attributeId, category);
+		deleteAttribute(category, attributeId);
 	}
 
-	private void deleteAttribute(String attributeId, Category category) throws BadRequestException {
-		CategoryAttribute catAttr = getCategoryAttributeWithException(attributeId, category);
+	private void deleteAttribute(Category category, String attributeId) throws BadRequestException {
+		CategoryAttribute catAttr = getCategoryAttributeWithException(category, attributeId);
 		deleteAttribute(category, catAttr);
 	}
 
@@ -43,7 +39,7 @@ public class CategoryService {
 		return category;
 	}
 
-	private CategoryAttribute getCategoryAttributeWithException(String attributeId, Category category)
+	private CategoryAttribute getCategoryAttributeWithException(Category category, String attributeId)
 			throws BadRequestException {
 		CategoryAttribute catAttr = category.getCategoryAttributes().stream()
 				.filter(ca -> ca.getId() == Long.valueOf(attributeId)).findFirst().orElse(null);
@@ -56,7 +52,19 @@ public class CategoryService {
 	@Transactional
 	public void deleteCategory(String id) throws BadRequestException {
 		Category category = getCategoryWithException(id);
-		category.getCategoryAttributes().forEach(catAttr -> deleteAttribute(category, catAttr));
 		catRepository.delete(category);
+	}
+
+	public Category updateCategory(String id, Category category) throws BadRequestException {
+		if (!category.getId().toString().equals(id)) {
+			throw new BadRequestException("ID from request is not matching provided category");
+		}
+		Category dbCat = catRepository.getOne(category.getId());
+		if (dbCat == null) {
+			throw new BadRequestException("Category does not exist");
+		}
+		dbCat.setName(category.getName());
+		dbCat.setMultipleChoice(category.isMultipleChoice());
+		return catRepository.save(dbCat);
 	}
 }
